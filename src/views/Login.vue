1840996b5fb6eac0c615066f2ae5a4b8da716ed7
songwufan <396989406@qq.com>
2020-10-11 18:49:00 +0800

    Initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <!--flex弹性盒子模型，justify-content：主抽 -->
+    <div style="display: flex;justify-content: center;margin-top: 150px">
+      <el-card style="width: 500px">
+    <el-container>
+      <el-header>
+        <img class="logo" src="https://shopsite.oss-cn-shenzhen.aliyuncs.com/logo.jpg" alt="myblog">
+      </el-header>
+      <el-main>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即登录</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        username: 'wufan11',
+        password: '111111'
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+          {min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'change'}
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const _this = this
+          this.$axios.post('/login',this.ruleForm).then(res => {
+            const jwt = res.headers['authorization']
+            const userInfo = res.data.data
+            //数据共享
+            _this.$store.commit('SET_TOKEN',jwt)
+            _this.$store.commit('SET_USERINFO',userInfo)
+            //获取
+            _this.$router.push('/blogs')
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-header {
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.logo {
+  height: 97%;
+  margin-top: 1px;
+}
+.demo-ruleForm{
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
